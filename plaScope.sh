bdb83dcbee7566540b55fef52d1c23e9cee0285a
GuilhemRoyer <guilhem.royer@gmail.com>
2018-05-31 10:03:35 +0200

    Update plaScope.sh

--- plaScope.sh
+++ plaScope.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
 
+# Copyright 2018, Guilhem Royer <groyer@genoscope.cns.fr>
+#
+# This file is part of PlaScope.
+#
+# PlaScope is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PlaScope is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PlaScope.  If not, see <http://www.gnu.org/licenses/>.
+
 ##############################################################
 # Author Guilhem ROYER groyer@genoscope.cns.fr | 06/04/2018  #
 ##############################################################
