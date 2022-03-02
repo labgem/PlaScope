f32511bbbad10eb9829d25e3a5fee995c0ea5d36
Julian Paganini <j.a.paganini@umcutrecht.nl>
2022-03-02 17:53:17 +0100

    Considered separetly processing of files with spades and unicycler headers

--- format_results_gplas.sh
+++ format_results_gplas.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-while getopts :i:o: flag; do
+while getopts :i:o:a:p: flag; do
 	case $flag in
 		i) input_file=$OPTARG;;
-                o) output_file=$OPTARG
+                o) output_file=$OPTARG;;
+                p) plascope_directory=$OPTARG;;
+                a) assembler=$OPTARG
 	esac
 done
 
@@ -11,20 +13,40 @@ done
 #1. Create file with proper header to hold the results
 echo -e '"Prob_Chromosome"\t"Prob_Plasmid"\t"Prediction"\t"Contig_name"\t"Contig_length"' > ${output_file}
 #2. Cat the Plascope results and process line by line
-cat ${input_file} | while read line
+if [[ $assembler == 'unicycler' ]]
+then
+tail -n+2 ${input_file} | while read line
 do
-classification=$(echo $line | cut -f 2 -d ' ')
-contig=$(echo $line | cut -f 1 -d ' ')
-length=$(echo $contig | cut -f 3 -d : | cut -f 1 -d _ )
-if [ $classification = 'plasmid' ]
+classification=$(echo $line | cut -f 3 -d ' ')
+contig_number=$(echo $line | cut -f 1 -d ' ')
+echo 'contig_number',${contig_number}
+contig=$(grep -w '>'${contig_number} ${plascope_directory}*fasta | cut -f 2 -d : | sed 's/>//g' )
+length=$(echo $line | cut -f 7 -d ' ')
+if [ $classification == '3' ]
 then
 echo -e 0'\t'1'\t''"Plasmid"''\t''"'$contig'"''\t'$length >> ${output_file}
-elif [ $classification = 'chromosome' ]
+elif [ $classification == '2' ]
 then
 echo -e 1'\t'0'\t''"Chromosome"''\t''"'$contig'"''\t'$length >> ${output_file}
-elif [ $classification = 'unclassified' ]
-then
+else
 echo -e 0.5'\t'0.5'\t''"Plasmid"''\t''"'$contig'"''\t'$length >> ${output_file}
 fi
 done
 
+else
+tail -n+2 ${input_file} | while read line
+do
+classification=$(echo $line | cut -f 3 -d ' ')
+contig=$(echo $line | cut -f 1 -d ' ')
+length=$(echo $line | cut -f 7 -d ' ')
+if [ $classification == '3' ]
+then
+echo -e 0'\t'1'\t''"Plasmid"''\t''"'$contig'"''\t'$length >> ${output_file}
+elif [ $classification == '2' ]
+then
+echo -e 1'\t'0'\t''"Chromosome"''\t''"'$contig'"''\t'$length >> ${output_file}
+else                                     
+echo -e 0.5'\t'0.5'\t''"Plasmid"''\t''"'$contig'"''\t'$length >> ${output_file}
+fi
+done
+fi
